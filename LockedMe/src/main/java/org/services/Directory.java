a1eff0b91d4648a7c0dd077e75585880762a70ad
Gade Tech <gadelock@gmail.com>
2022-04-29 20:37:42 -0400

    Add file implemented

--- LockedMe/src/main/java/org/services/Directory.java
+++ LockedMe/src/main/java/org/services/Directory.java
@@ -1,17 +1,20 @@
 package org.services;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Directory {
     private List<File> files;
-    public static final String directoryName = "../Storage/";
+    public String directoryName;
 
-    public Directory() {
+    public Directory(String folderPath) {
+        directoryName = folderPath;
         //create instance of the FileSystems. Converting the directory path to the absolute path.
         Path path = FileSystems.getDefault().getPath(directoryName).toAbsolutePath();
         //read the folder
@@ -19,23 +22,56 @@ public class Directory {
         //read all the files in the folder
         File[] dirFiles = folder.listFiles();
         //store files into the list
-        files = Arrays.asList(dirFiles);
+        files = new ArrayList<>(Arrays.asList(dirFiles));
         Collections.sort(files);
     }
 
-    public List<File> getSortedFiles(){
+    public List<File> getSortedFiles() {
         return files;
     }
 
-    public void printFiles(){
+    public void printFiles() {
         System.out.println("All files in " + directoryName);
         for (File f : files) {
             System.out.println(f);
         }
     }
 
+    /**
+     * Add the file to the current directory
+     *
+     * @param fileName the name of the file to add
+     * @return True if file is new and successfully added. False otherwise.
+     */
+    public boolean addFile(String fileName) {
+        Path path = FileSystems.getDefault().getPath(directoryName + fileName).toAbsolutePath();
+        File f = path.toFile();
+        try {
+            if (f.createNewFile()) {
+                files.add(f);
+                Collections.sort(files);
+                System.out.println("File successfully created.");
+                return true;
+            } else {
+                System.out.println("File already existed.");
+                return false;
+            }
+        } catch (IOException e) {
+            System.out.println("IO exception occurred.");
+            return false;
+        }
+    }
+
     public static void main(String[] args) {
-        Directory d = new Directory();
+        Directory d = new Directory("../Storage/");
+        System.out.println("------------------------");
+        System.out.println("Print all files in sorting order.");
+        d.printFiles();
+        System.out.println("------------------------");
+        System.out.println("Adding a new file.");
+        d.addFile("Test3.txt");
+        System.out.println("------------------------");
+        System.out.println("Print all files in sorting order.");
         d.printFiles();
     }
 }
